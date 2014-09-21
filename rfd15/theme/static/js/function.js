8ffe588eee3204fc1950d33add57008999d93fb2
Paul Roeland <paul@cleanclothes.org>
2014-09-21 14:39:13 +0200

    suppress theme for wildcard.foldercontents

--- rfd15/theme/static/js/function.js
+++ rfd15/theme/static/js/function.js
@@ -1,22 +1,25 @@
 jQuery(document).ready(function() {
-    
+
 jQuery(document).ready(function(jq) {
 
-var buscalink = document.location.href + '/search'; 
+var buscalink = document.location.href + '/search';
 
 jQuery("#searchbt a").attr({
           href:buscalink
         });
 
-}); 
-	 
+});
+
 jQuery('#searchbt a').prepOverlay({
                 subtype: 'ajax',
                 filter: '#content>*',
                 });
-
-
-				
-				
-
+});
+jQuery( function($) {
+  $('img.image-right, img.image-left, img.image-inline, img.eventImage, img.newsImage')
+    .prepOverlay({
+      subtype: 'image',
+      urlmatch: '/image_.+$',
+      urlreplace: '/image_large'
+      });
 });
